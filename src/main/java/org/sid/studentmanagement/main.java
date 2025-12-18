922c01cfecc831cf8bcbfd6051c273c821903928
EL YAACOUBI-MOUAD73 <drolaamy@gmail.com>
2025-12-18 09:52:24 +0100

    Update main.java

--- src/main/java/org/sid/studentmanagement/main.java
+++ src/main/java/org/sid/studentmanagement/main.java
@@ -1,18 +1,14 @@
-package org.sid.studentmanagement;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+package com.example;
 
 public class Main {
-
-    private static final Logger logger =
-            LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
 
         StudentService service = new StudentService();
 
-        logger.info("Application started");
-        logger.info("Average = {}", service.calculateAverage());
+        System.out.println(service.average()); 
+        service.addStudent(null);               
+        service.findStudent(null);               
     }
 }
+
