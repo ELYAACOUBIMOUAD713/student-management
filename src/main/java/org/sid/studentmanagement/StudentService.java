c8e4604357f3e682813b32d5fbe5af2be3963223
EL YAACOUBI-MOUAD73 <drolaamy@gmail.com>
2025-12-18 09:52:55 +0100

    Update StudentService.java

--- src/main/java/org/sid/studentmanagement/StudentService.java
+++ src/main/java/org/sid/studentmanagement/StudentService.java
@@ -1,11 +1,11 @@
-package org.sid.studentmanagement;
+package com.example;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class StudentService {
 
-    private final List<Student> students = new ArrayList<>();
+    private List<Student> students = new ArrayList();
 
     public void addStudent(Student s) {
         students.add(s); // null possible
