d8ba4f8fbe44d9383a856d852d1ecb21cafa7278
EL YAACOUBI-MOUAD73 <drolaamy@gmail.com>
2025-12-18 09:24:01 +0100

    Update main.java

--- src/main/java/org/sid/studentmanagement/main.java
+++ src/main/java/org/sid/studentmanagement/main.java
@@ -1,1 +1,13 @@
+package org.sid.studentmanagement;
 
+public class Main {
+
+    public static void main(String[] args) {
+
+        StudentService service = new StudentService();
+
+        System.out.println(service.average()); // BUG volontaire
+        service.addStudent(null);               // BUG
+        service.findStudent(null);               // BUG
+    }
+}
